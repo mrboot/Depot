4e4eb4ccc62e0bf37a8a50c6f6b359634069717f
Mark Booth <mrbooth@gmail.com>
2012-02-16 18:23:47 +0000

    initial parts of admin login - users, sessions & admin

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'jquery-rails'
 gem 'will_paginate', '~> 3.0'
 
 # To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # Use unicorn as the web server
 # gem 'unicorn'
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+  
+end
